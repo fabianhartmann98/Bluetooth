f1c7f0582f93042fd11603b6dd9a322dcdfe7a1b
Fabian Hartmann <fabianhartmann98@gmail.com>
2016-10-20 16:23:59 +0200

    continiud server client mode (Bluetooth Listener)

--- Bluetooth/MainWindow.xaml.cs
+++ Bluetooth/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using InTheHand.Net.Sockets;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,10 +24,16 @@ namespace Bluetooth
     public partial class MainWindow : Window
     {
         BluetoothClient bc;
-        BluetoothDeviceInfo[] infos; 
+        BluetoothDeviceInfo[] infos;
+        Stream s;
+        BluetoothListener bl; 
         public MainWindow()
         {
             InitializeComponent();
+            string [] typ = {"SERVER","CLIENT"};
+            Type.ItemsSource = typ;
+            Type.SelectedIndex = 0; 
+
             bc = new BluetoothClient();
 
             infos = bc.DiscoverDevices(255,false,true,true);
@@ -38,13 +45,23 @@ namespace Bluetooth
             Devices.ItemsSource = names;
             Devices.SelectedIndex = 0;
         }
-
         
 
         private void Connect_ac(IAsyncResult ar)
         {
             if (ar.IsCompleted)
                 MessageBox.Show("Connected");
+            
+            s = bc.GetStream(); 
+        }
+
+        private void Listener_ac(IAsyncResult ar)
+        {
+            if (ar.IsCompleted)
+                MessageBox.Show("Connected");
+
+            bc = bl.EndAcceptBluetoothClient(ar);
+            s = bc.GetStream(); 
         }
 
         private void Connect_Click(object sender, RoutedEventArgs e)
@@ -55,7 +72,7 @@ namespace Bluetooth
                 Guid serviceClass = Guid.NewGuid();
                 BluetoothDeviceInfo device = infos[i];
                 BluetoothSecurity.PairRequest(device.DeviceAddress, "0000");
-
+                
                 if (device.Authenticated)
                 {
                     bc.SetPin("0000");
@@ -74,5 +91,24 @@ namespace Bluetooth
         {
 
         }
+
+        private void Type_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Type.SelectedValue== "CLIENT")
+            {
+                Connect.IsEnabled = false;
+                bl= new BluetoothListener(new Guid());
+                bl.BeginAcceptBluetoothClient(new AsyncCallback(Connect_ac), bl); 
+            }
+            else
+            {
+                if (bl != null)
+                {
+                    bl.Stop(); 
+                }
+            }
+        }
+
+
     }
 }
