ccff895a8016478ab9cf8629832f20a3a6f13559
Fabian Hartmann <fabianhartmann98@gmail.com>
2016-11-23 21:04:00 +0100

    creating serial port

--- Bluetooth/MainWindow.xaml.cs
+++ Bluetooth/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using InTheHand.Net.Sockets;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,12 +26,19 @@ namespace Bluetooth
     {
         BluetoothClient bc;
         BluetoothDeviceInfo[] infos;
-        Stream s;
-        BluetoothListener bl; 
+        SerialPort sp = new SerialPort();
+        string[] ports;
+
+        private int baudrate = 38400;
+        private StopBits stopbits = StopBits.One;
+        private int databits = 8;
+        private Parity parity = Parity.None; 
+
         public MainWindow()
         {
             InitializeComponent();
-            
+
+            ports = SerialPort.GetPortNames(); 
 
             bc = new BluetoothClient();
 
@@ -48,9 +56,51 @@ namespace Bluetooth
         private void Connect_ac(IAsyncResult ar)
         {
             if (ar.IsCompleted)
-                MessageBox.Show("Connected");
-            
-            s = bc.GetStream(); 
+                if (bc.Connected)
+                {
+                    MessageBox.Show("Connected");
+                    bc.Close();
+                    string[] newports = SerialPort.GetPortNames();
+                    string addedport = "";
+                    foreach (var item in newports)
+                    {
+                        bool IsAnOldOne = false;
+                        foreach (var item2 in ports)
+                        {
+                            if(item==item2)
+                            {
+                                IsAnOldOne=true;
+                                break;
+                            }
+                        }
+                        if (!IsAnOldOne)
+                        {
+                            addedport = item;
+                            break;
+                        }
+                    }
+                    settingUpSerialPort("COM6");
+                    sp.Open(); 
+                }
+
+        }
+
+        private void settingUpSerialPort(string Port)
+        {
+            if (sp.IsOpen)
+                sp.Close();
+            sp = new SerialPort();
+            sp.BaudRate = baudrate;
+            sp.StopBits = stopbits;
+            sp.DataBits = databits;
+            sp.Parity = parity;
+            sp.PortName = Port;            
+            sp.DataReceived += sp_DataReceived;
+        }
+
+        private void sp_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
 
@@ -62,7 +112,7 @@ namespace Bluetooth
                 Guid serviceClass = Guid.NewGuid();
                 BluetoothDeviceInfo device = infos[i];
 
-                device.SetServiceState(BluetoothService.SerialPort, true, true);
+                device.SetServiceState(BluetoothService.SerialPort, true);
 
                 BluetoothSecurity.PairRequest(device.DeviceAddress, "0000");
                 
@@ -81,7 +131,7 @@ namespace Bluetooth
 
         private void Send_b_Click(object sender, RoutedEventArgs e)
         {
-
+            sp.Write("done");
         }
 
 
